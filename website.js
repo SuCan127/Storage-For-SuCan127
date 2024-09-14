901849ed1c985872863c4bd76907e64abdd980d0
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-14 19:41:19 +0800

    Update website.js

--- website.js
+++ website.js
@@ -8,8 +8,9 @@ headers['Content-Security-Policy'] = '';
 let reg = /<head>/;
 let str = `<head>
   <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover">
+  <meta name="apple-mobile-web-app-status-bar-style" content="default">
+  <meta name="viewport" content="user-scalable=no,initial-scale=1.0,maximum-scale=1.0" />
+  <meta name="apple-mobile-web-app-title" content="调试1">
 `;
 
 body = body.replace(reg, str);
