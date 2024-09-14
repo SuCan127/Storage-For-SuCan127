ebd6e5bbc5413aad616282c0d57a3ba41c83d306
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-14 20:05:31 +0800

    Update website.js

--- website.js
+++ website.js
@@ -7,23 +7,11 @@ headers['Content-Security-Policy'] = '';
 // 在 <head> 标签后面插入 <meta> 标签和处理链接跳转的 JavaScript
 let reg = /<head>/;
 let str = `<head>
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="default">
-  <meta name="viewport" content="user-scalable=no,initial-scale=1.0,maximum-scale=1.0" />
   <meta name="apple-mobile-web-app-title" content="调试1">
-  <script>
-    // 防止 Safari 工具栏弹出并处理链接行为
-    document.addEventListener('DOMContentLoaded', function() {
-      document.querySelectorAll('a').forEach(link => {
-        link.addEventListener('touchstart', function(event) {
-          event.preventDefault(); // 阻止默认的链接行为，包括工具栏的显示
-          setTimeout(() => {
-            window.location.href = link.href; // 在当前 WebApp 中导航到新页面
-          }, 100); // 添加轻微的延迟避免刷新错误
-        });
-      });
-    });
-  </script>
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width,initial-scale=1, minimum-scale=1.0, maximum-scale=1, user-scalable=no">
 `;
 
 body = body.replace(reg, str);
