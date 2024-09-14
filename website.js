4c1524bc717746e2cce5e43083e9fc8782795664
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-14 21:22:33 +0800

    Update website.js

--- website.js
+++ website.js
@@ -1,15 +1,13 @@
 let body = $response.body;
 let headers = $response.headers;
 
-// 删除 CSP 头部限制，绕过安全策略
 headers['Content-Security-Policy'] = '';
 
-// 在 <head> 标签后面插入 <meta> 标签和处理链接跳转的 JavaScript
 let reg = /<head>/;
 let str = `<head>
-  <meta name="apple-mobile-web-app-title" content="调试1">
+  <meta name="apple-mobile-web-app-title" content="山无涯">
   <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-status-bar-style" content="default">
   <meta name="format-detection" content="telephone=no">
   <meta name="viewport" content="width=device-width,initial-scale=1, minimum-scale=1.0, maximum-scale=1, user-scalable=no">
 `;
