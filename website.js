c40a04e770af70d1117730f7ee7eab43ba82504c
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-14 19:49:44 +0800

    Update website.js

--- website.js
+++ website.js
@@ -10,14 +10,16 @@ let str = `<head>
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="default">
   <meta name="viewport" content="user-scalable=no,initial-scale=1.0,maximum-scale=1.0" />
-  <meta name="apple-mobile-web-app-title" content="调试2">
+  <meta name="apple-mobile-web-app-title" content="调试3">
   <script>
-    // 防止 Safari 工具栏弹出
+    // 防止 Safari 工具栏弹出并处理链接行为
     document.addEventListener('DOMContentLoaded', function() {
       document.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', function(event) {
-          event.preventDefault(); // 阻止默认的链接行为
-          window.location.href = link.href; // 在当前窗口中导航到新页面
+        link.addEventListener('touchstart', function(event) {
+          event.preventDefault(); // 阻止默认的链接行为，包括工具栏的显示
+          setTimeout(() => {
+            window.location.href = link.href; // 在当前 WebApp 中导航到新页面
+          }, 100); // 添加轻微的延迟避免刷新错误
         });
       });
     });
@@ -28,4 +30,3 @@ body = body.replace(reg, str);
 
 // 返回修改后的响应体和响应头
 $done({body, headers});
-
