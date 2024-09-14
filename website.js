8aaadcd79b9bb3da991020ac00e7b2e7c7b04ee3
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-14 19:13:26 +0800

    Update website.js

--- website.js
+++ website.js
@@ -10,6 +10,8 @@ let str = `<head>
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="default">
   <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover">
+  <meta name="apple-mobile-web-app-title" content="山无涯">
+  <meta name="theme-color" content="#000000">
 `;
 
 body = body.replace(reg, str);
