a9c527b53a233af0b915114be25ccd0fe04e4cfc
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-14 19:34:26 +0800

    Update website.js

--- website.js
+++ website.js
@@ -10,11 +10,6 @@ let str = `<head>
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
   <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover">
-  // <meta name="apple-touch-fullscreen" content="yes">
-  // <meta name="HandheldFriendly" content="true">
-  // <meta name="screen-orientation" content="portrait">
-  // <meta name="full-screen" content="yes">
-  // <meta name="browsermode" content="application">
 `;
 
 body = body.replace(reg, str);
