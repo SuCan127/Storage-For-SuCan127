e26127422e4fc939950f07aab5e8c8fa714b7162
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-14 18:52:02 +0800

    Create website.js

---
+++ website.js
@@ -0,0 +1,18 @@
+let body = $response.body;
+let headers = $response.headers;
+
+// 删除 CSP 头部限制，绕过安全策略
+headers['Content-Security-Policy'] = '';
+
+// 在 <head> 标签后面插入 <meta> 标签
+let reg = /<head>/;
+let str = `<head>
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="default">
+  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover">
+`;
+
+body = body.replace(reg, str);
+
+// 返回修改后的响应体和响应头
+$done({body, headers});
