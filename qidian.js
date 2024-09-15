f17c4f75e0e62c5544f214f5a881f72a3f17e4c5
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-15 16:49:04 +0800

    Update qidian.js

--- qidian.js
+++ qidian.js
@@ -3,7 +3,4 @@ let body = $response.body;
 body = body.replace('<div id="reader-content"', 
                     '<div id="reader-content" style="position: absolute; left: 0; width: 100%;"');
 
-body = body.replace('<div id="right-container"', 
-                    '<div id="right-container" style="position: absolute; left: 0; width: 100%;"');
-
 $done({ body });
