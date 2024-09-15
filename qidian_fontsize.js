6140e6c1e317d8814bb4511c2418d634a42ee457
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-15 15:36:16 +0800

    Update qidian_fontsize.js

--- qidian_fontsize.js
+++ qidian_fontsize.js
@@ -1,20 +1,20 @@
-// Quantumult X script to change the font size of specific elements to 100px
+// Quantumult X script to insert CSS and JavaScript into the response HTML
+let rHead = '<head>';
+let newStyle = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/baidu.zhidao.css" type="text/css">';
+let rBody = '</body>';
+let newJavaScript = '<script type="text/javascript" async="async" src="//limbopro.com/Adguard/baidu.zhidao.js"></script></body>';
 
-(function() {
-    let modifyFontSize = () => {
-        // Target the elements based on the data attribute or class names
-        let targetElements = document.querySelectorAll('[data-v-8a7b341d] li');
-        
-        // Change the font size of the targeted elements to 100px
-        targetElements.forEach(element => {
-            element.style.fontSize = '100px';
-        });
-    };
-
-    // Run the function after the page has fully loaded
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', modifyFontSize);
-    } else {
-        modifyFontSize();
-    }
-})();
+// 确保 response.body 存在，并执行替换操作
+if ($response.body) {
+    // 将 <head> 标签替换为带有新样式表的内容
+    let body = $response.body.replace(rHead, newStyle);
+    
+    // 将 </body> 标签替换为带有 JavaScript 的内容
+    body = body.replace(rBody, newJavaScript);
+    
+    // 返回修改后的 body 内容
+    $done({ body: body });
+} else {
+    // 如果没有 body 内容，则直接返回未修改的响应
+    $done({});
+}
