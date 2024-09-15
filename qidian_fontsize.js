0d7d0f15d4904d52e6d8dcb5e45472b936629265
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-15 16:17:42 +0800

    Update qidian_fontsize.js

--- qidian_fontsize.js
+++ qidian_fontsize.js
@@ -2,8 +2,8 @@ let body = $response.body;
 
 body = body.replace('<div id="reader-content"', '<div id="reader-content" style="position: absolute; left: 0; width: 100%;"');
 
-body = body.replace('<section data-v-63a3e543="" id="side-sheet" class="h-full"', '<section data-v-63a3e543="" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999;"');
+body = body.replace('<section data-v-63a3e543="" id="side-sheet" class="h-full"', '<section style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999;"');
 
-body = body.replace('<div data-v-47ffe1ec class="pl-12px pb-136px z-4 absolute left-full pt-12px" id="r-menu"', '<div data-v-47ffe1ec style="position: fixed; bottom: 0;"');
+body = body.replace('<div data-v-47ffe1ec class="pl-12px pb-136px z-4 absolute left-full pt-12px" id="r-menu"', '<div style="position: fixed; bottom: 0;"');
 
 $done({ body });
