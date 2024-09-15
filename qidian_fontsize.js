bd31f66a1c2d4e7e0188935db756235cb3814a4d
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-15 14:02:16 +0800

    Update qidian_fontsize.js

--- qidian_fontsize.js
+++ qidian_fontsize.js
@@ -1,15 +1,11 @@
 let body = $response.body;
 
-// 插入样式标签，修改字体大小为21px
-let newStyle = `
-<style>
-  body {
-    font-size: 100px !important;
-  }
-</style>
+// 插入 viewport 元标签，使得页面在手机上适配
+let newViewport = `
+<meta name="viewport" content="width=device-width, initial-scale=1.5, user-scalable=no">
 `;
 
-// 将样式插入到 <head> 标签中
-body = body.replace(/<head>/, `<head>${newStyle}`);
+// 将 viewport 元标签插入到 <head> 标签中
+body = body.replace(/<head>/, `<head>${newViewport}`);
 
 $done({ body });
