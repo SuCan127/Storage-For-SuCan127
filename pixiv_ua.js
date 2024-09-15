014716ca73351b98af977a8fcf97657def3fefef
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-15 15:12:52 +0800

    Update pixiv_ua.js

--- pixiv_ua.js
+++ pixiv_ua.js
@@ -1,14 +1,16 @@
 // Pixiv PC User-Agent 替换脚本
 
-// 检查请求的 URL 是否匹配
-if ($request.url.indexOf("/tags/") !== -1 && $request.url.indexOf("/artworks") !== -1) {
-    // 替换 User-Agent 为桌面版的
-    var modifiedHeaders = $request.headers;
+// 确认当前请求是否为 Pixiv 相关的 tags 和 artworks 页面
+if ($request.url.includes("/tags/") && $request.url.includes("/artworks")) {
+    // 克隆当前请求头
+    let modifiedHeaders = Object.assign({}, $request.headers);
+    
+    // 修改 User-Agent 为桌面版
     modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36';
 
-    // 返回新的请求信息
+    // 返回修改后的请求信息
     $done({ headers: modifiedHeaders });
 } else {
-    // 如果 URL 不匹配则不做任何修改
+    // 如果 URL 不符合要求，直接不做修改
     $done({});
 }
