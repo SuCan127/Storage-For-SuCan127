d2b1cc5da3714aba155416b2ed7a53d11add42d6
Nie haodong <65551049+SuCan127@users.noreply.github.com>
2024-09-15 15:44:33 +0800

    Update qidian_fontsize.js

--- qidian_fontsize.js
+++ qidian_fontsize.js
@@ -1,11 +1,11 @@
 let reg = '<div id="right-container" class="h-100vh fixed top-0 flex items-end z-2 transition-transform duration-300" style="left: 1363px;">';
-let newDiv = '<div id="right-container" class="h-100vh fixed top-0 flex items-end z-2 transition-transform duration-300" style="left: 0; width: 100%; height: 100%;">';
+let newDiv = '<div id="right-container" class="h-100vh fixed top-0 flex items-end z-2 transition-transform duration-300" style="left: 0; width: 120%; height: 120%;">';
 
-// 替换响应体中的原始 `#right-container` 容器，使其占满全屏
+// 替换响应体中的原始 `#right-container` 容器，增加其宽度和高度
 if ($response.body) {
     let body = $response.body.replace(reg, newDiv);
     $done({ body });
 } else {
-    $done({});
+    let body = $response.body.replace(reg, newDiv);
+    $done({ body });
 }
-
